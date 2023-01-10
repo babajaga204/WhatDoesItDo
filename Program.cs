dfd53197631807d4af1994bd8b16a94a34e205b8
Simen Kristiansen <simenbolstad@gmail.com>
2023-01-10 16:46:40 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace WhatDoesItDo;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ReadTextAndShowCharCount(250);
+    }
+
+    private static void ReadTextAndShowCharCount(int range)
+    {
+        var counts = new int[range];
+        string text = "something";
+        while (!string.IsNullOrWhiteSpace(text))
+        {
+            text = Console.ReadLine();
+            UpdateCharacters(counts, text);
+            ShowCounts(range, counts);
+        }
+    }
+
+    private static void ShowCounts(int range, int[] counts)
+    {
+        for (var i = 0; i < range; i++)
+        {
+            if (counts[i] > 0)
+            {
+                var character = (char)i;
+                Console.WriteLine("\"" + character + "\" - " + counts[i]);
+            }
+        }
+    }
+
+    private static void UpdateCharacters(int[] counts, string text)
+    {
+        foreach (var character in text ?? string.Empty)
+        {
+            counts[(int)character]++;
+        }
+    }
+}
