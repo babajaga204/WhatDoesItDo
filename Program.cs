ca12cb5979f2a00a824f7798e0e0ab42dffa85d6
Simen Kristiansen <simenbolstad@gmail.com>
2023-01-10 17:49:21 +0100

    Added percentage count and right-side orientation. NOT FINISHED

--- Program.cs
+++ Program.cs
@@ -10,29 +10,34 @@
     private static void ReadTextAndShowCharCount(int range)
     {
         var counts = new int[range];
+        int totalLetters = 0;
         string text = "something";
         while (!string.IsNullOrWhiteSpace(text))
         {
             text = Console.ReadLine().ToLower();
-            UpdateCharacters(counts, text);
-            ShowCounts(range, counts);
+            UpdateCharacters(counts, text, totalLetters);
+            ShowCounts(range, counts, totalLetters);
         }
     }
 
-    private static void ShowCounts(int range, int[] counts)
+    private static void ShowCounts(int range, int[] counts, int totalLetters)
     {
         for (var i = 0; i < range; i++)
         {
             if (counts[i] <= 0) continue;
             var character = (char)i;
-            Console.WriteLine($"\"{character}\" - {counts[i]}");
+            var percentage = 100 * (double)counts[i] / totalLetters;
+            string output = character + " - " + percentage.ToString("P") + "%";
+            Console.CursorLeft = Console.BufferWidth - output.Length - 1;
+            Console.WriteLine(output);
         }
     }
 
-    private static void UpdateCharacters(int[] counts, string text)
+    private static void UpdateCharacters(int[] counts, string text, int totalLetters)
     {
         foreach (var character in text ?? string.Empty)
         {
+            totalLetters++;
             counts[(int)character]++;
         }
     }
